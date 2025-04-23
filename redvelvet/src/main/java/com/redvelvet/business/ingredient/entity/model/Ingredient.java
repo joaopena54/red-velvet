41535aaf9b1220fcfbdc3fce950202ac9d59778c
João Pena <joaopena5454@gmail.com>
2025-04-23 22:47:11 +0100

    startup

--- redvelvet/src/main/java/com/redvelvet/business/ingredient/entity/model/Ingredient.java
+++ redvelvet/src/main/java/com/redvelvet/business/ingredient/entity/model/Ingredient.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Ingredient extends BaseEntity {
 
     @Column(nullable = false)
